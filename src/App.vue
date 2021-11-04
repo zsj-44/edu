e2499ae46e5aef15ff259b15708b77d7c484b38a
zsj <zsj13637352824@gmail.com>
2021-11-04 16:57:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    你好呀
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/views/course/audio.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>音频</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
